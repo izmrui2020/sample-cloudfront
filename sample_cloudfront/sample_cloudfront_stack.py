ec39c03d523beceb3da3fa86419bd3d3bbaf1091
izmrui2020 <rui.iizumi.20@gmail.com>
2022-11-01 08:02:34 +0900

    first commit

--- sample_cloudfront/sample_cloudfront_stack.py
+++ sample_cloudfront/sample_cloudfront_stack.py
@@ -2,10 +2,7 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_cloudfront as cf,
 )
 
 
@@ -14,13 +11,36 @@ class SampleCloudfrontStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "SampleCloudfrontQueue",
-            visibility_timeout=Duration.seconds(300),
+        # my_distribution = cf.Distribution(self, "MyCfWebDistribution",
+        #     default_behavior=cf.BehaviorOptions(
+        #         ##origin=cf.IOrigin(origin_id="arn:aws:s3:::cloudfronttest-sample-bucket")
+        #     )
+        # )
+        
+        origin_property = cf.CfnDistribution.OriginProperty(
+            domain_name=f"cloudfronttest-sample-bucket.s3.ap-northeast-1.amazonaws.com",
+            id="myorigin1",
+            # s3_origin_config=cf.CfnDistribution.S3OriginConfigProperty(
+            #     origin_access_identity="originAccessIdentity"
+            # ),
+            custom_origin_config=cf.CfnDistribution.CustomOriginConfigProperty(
+                origin_protocol_policy="https-only",
+            ),
         )
-
-        topic = sns.Topic(
-            self, "SampleCloudfrontTopic"
+        
+        cfn_distribution = cf.CfnDistribution(self, "HogeDistribution",
+            distribution_config=cf.CfnDistribution.DistributionConfigProperty(
+                comment="this is hoge",
+                enabled=True,
+                default_cache_behavior=cf.CfnDistribution.DefaultCacheBehaviorProperty(
+                    target_origin_id="myorigin1",
+                    viewer_protocol_policy="https-only",
+                    forwarded_values=cf.CfnDistribution.ForwardedValuesProperty(
+                        query_string=False,
+                    ),
+                ),
+                origins=[
+                    origin_property
+                ],
+            )
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
